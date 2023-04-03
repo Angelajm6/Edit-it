6525998ec733dadca136093c1ee69ed7c853d1db
angelajm6 <angelajaume.m@gmail.com>
2023-04-03 09:15:48 -0400

    added logic to putDb and getDb methods and added comments and logic to event handlers in install.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,7 +1,9 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+//Creating a new database names 'jate' which will be using version 1 of the database
   openDB('jate', 1, {
+     // Adding our database schema if it has not already been initialized.
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -12,10 +14,41 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+//Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  
+  try {
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+    const jateDB = await openDB('jate',1);
+    const tx = jateDB.transaction('jate','readwrite');
+    const currentContent = tx.objectStore('jate');
+    const request = currentContent.add({content});
+    const res = await request;
+  
+  } catch(err) {
+    console.error('putDb not implemented',err)
+  };
 
+};
+
+
+//Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  
+  try {
+
+    const jateDB = await openDB('jate',1);
+    const tx = jateDB.transaction('jate','readonly');
+    const currentNotes = tx.objectStore('jate');
+    const request = currentNotes.getAll();
+    const result = await request;
+
+  } catch(err) {
+    console.error('getDb not implemented',err)
+  }
+
+}; 
+
+
+// Start the database.
 initdb();
